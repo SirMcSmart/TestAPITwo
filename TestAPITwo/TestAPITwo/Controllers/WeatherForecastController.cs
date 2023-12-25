4d7516ab0ffb6638fb6c49f0ad0d68ddfbd9c7ec
McSmart <ofoma_chijioke@yahoo.com>
2023-12-25 11:27:07 +0100

    added a parameter

--- TestAPITwo/TestAPITwo/Controllers/WeatherForecastController.cs
+++ TestAPITwo/TestAPITwo/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace TestAPITwo.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "GetWeatherForecast/{weather}")]
+        public IEnumerable<WeatherForecast> Get(string weather)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
