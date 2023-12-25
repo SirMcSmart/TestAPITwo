d98b71c387c052138304d826b5846841ca4b0825
McSmart <ofoma_chijioke@yahoo.com>
2023-12-25 11:49:20 +0100

    Added another endpoint

--- TestAPITwo/TestAPITwo/Controllers/WeatherForecastController.cs
+++ TestAPITwo/TestAPITwo/Controllers/WeatherForecastController.cs
@@ -41,5 +41,17 @@ namespace TestAPITwo.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "GetCountryWeatherForecast")]
+        public IEnumerable<WeatherForecast> GetCountryWeatherForecast(string weather)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
